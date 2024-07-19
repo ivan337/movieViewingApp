107ee725b920527d7c0605927a47e1cf4c73a87f
Чернов Иван Владиславович <chernov-iv@keysystems.ru>
2024-07-19 15:16:24 +0300

    bug 0 EsLint +codestyle

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,44 +1,57 @@
 module.exports = {
+    // Указываем парсер для TypeScript
     parser: '@typescript-eslint/parser',
+    // Настройки парсера
     parserOptions: {
+        // Указываем версию ECMAScript
         ecmaVersion: 2021,
+        // Указываем тип исходного кода как модуль
         sourceType: 'module',
+        // Включаем поддержку JSX
         ecmaFeatures: {
             jsx: true,
         },
     },
+    // Настройки для плагинов
     settings: {
+        // Автоматическое определение версии React
         react: {
             version: 'detect',
         },
+        // Настройка резолвера для импортов
         'import/resolver': {
             node: {
+                // Указываем расширения файлов, которые будут использоваться при импортах
                 extensions: ['.js', '.jsx', '.ts', '.tsx'],
             },
         },
     },
+    // Базовые конфигурации ESLint и плагинов
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'prettier',
+        'eslint:recommended', // Базовые рекомендуемые правила ESLint
+        'plugin:@typescript-eslint/recommended', // Рекомендуемые правила для TypeScript
+        'plugin:react/recommended', // Рекомендуемые правила для React
+        'plugin:react-hooks/recommended', // Рекомендуемые правила для React Hooks
+        'plugin:prettier/recommended', // Интеграция Prettier с ESLint
+        'prettier', // Отключение правил ESLint, конфликтующих с Prettier
     ],
+    // Плагины, которые будут использоваться
     plugins: [
-        '@typescript-eslint',
-        'react',
-        'react-hooks',
-        'prettier',
-        'import',
+        '@typescript-eslint', // Плагин для TypeScript
+        'react', // Плагин для React
+        'react-hooks', // Плагин для React Hooks
+        'prettier', // Плагин для Prettier
+        'import', // Плагин для сортировки импортов
     ],
+    // Правила ESLint
     rules: {
-        'react/react-in-jsx-scope': 'off',
-        'prettier/prettier': 'error',
-        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+        'react/react-in-jsx-scope': 'off', // Отключаем правило, требующее импорта React в файлах с JSX
+        'prettier/prettier': 'error', // Включаем Prettier как ESLint правило
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }], // Разрешаем использование JSX только в файлах с расширением .tsx
         'import/order': [
             'error',
             {
+                // Группы импортов для сортировки
                 groups: [
                     'builtin',
                     'external',
@@ -63,7 +76,8 @@ module.exports = {
             },
         ],
     },
+    // Среда выполнения
     env: {
-        node: true,
+        node: true, // Включаем поддержку Node.js
     },
 };
